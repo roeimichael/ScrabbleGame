47512035577cc0d10290ea84c44117a3de108d8b
roeimichael <roeym111@gmail.com>
2023-05-16 11:28:43 +0300

    added functionality for letter list

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -3,9 +3,14 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import java.util.Observable;
+import java.util.Observer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+import javafx.scene.input.*;
+import javafx.collections.FXCollections;
 import javafx.scene.layout.GridPane;
 
 import viewmodel.ViewModel;
@@ -18,7 +23,10 @@ public class MainWindowController implements Observer  {
 
     @FXML
     private Button restartButton;
-
+    
+    @FXML
+    private ListView<String> letterList;
+    
     @FXML
     private GridPane gameBoard;
     
@@ -27,6 +35,10 @@ public class MainWindowController implements Observer  {
     @FXML
     public void initialize() {
         slots = new TextField[15][15];
+        
+        // Let's say these are the letters available
+        letterList.setItems(FXCollections.observableArrayList("A", "B", "C", "D", "E"));
+        
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
                 TextField tf = new TextField();
@@ -34,8 +46,39 @@ public class MainWindowController implements Observer  {
                 tf.setPrefHeight(30); // Set preferred height as per your requirement
                 slots[i][j] = tf;
                 gameBoard.add(tf, j, i);
+
+                // Allow dropping a letter onto the text field
+                tf.setOnDragOver(event -> {
+                    if (event.getGestureSource() != tf && event.getDragboard().hasString()) {
+                        event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                    }
+                    event.consume();
+                });
+                
+                tf.setOnDragDropped(event -> {
+                    Dragboard db = event.getDragboard();
+                    boolean success = false;
+                    if (db.hasString()) {
+                        tf.setText(db.getString());
+                        success = true;
+                    }
+                    event.setDropCompleted(success);
+                    event.consume();
+                });
             }
         }
+        
+        // Allow dragging a letter from the list
+        letterList.setOnDragDetected(event -> {
+            String letter = letterList.getSelectionModel().getSelectedItem();
+            if (letter != null) {
+                Dragboard db = letterList.startDragAndDrop(TransferMode.ANY);
+                ClipboardContent content = new ClipboardContent();
+                content.putString(letter);
+                db.setContent(content);
+            }
+            event.consume();
+        });
     }
     
     public void setViewModel(ViewModel vm) {
@@ -60,6 +103,6 @@ public class MainWindowController implements Observer  {
 	
     @Override
     public void update(Observable o, Object arg) {
-        // TODO Auto-generated method stub
-    }
+    	
+	    }
 }
--- src/viewmodel/ViewModel.java
+++ src/viewmodel/ViewModel.java
@@ -6,14 +6,14 @@ import java.util.Observer;
 import model.Model;
 import view.MainWindowController;
 
-public class ViewModel implements Observer {
+public class ViewModel extends Observable implements Observer {
 	Model m;
 	  public IntegerProperty inputkey = new SimpleIntegerProperty();
 	public ViewModel(Model m) {
 		this.m = m;
 		m.addObserver(this);
 		
-		inputkey.addListener((o,ov,nv)->m.setInputKey((int)nv));
+//		inputkey.addListener((o,ov,nv)->m.setInputKey((int)nv));
 	}
 	 public void makeMove(String move) {
 	        m.updateBoardState(move);
@@ -21,10 +21,9 @@ public class ViewModel implements Observer {
 
 	 @Override
 	    public void update(Observable obs, Object obj) {
-	        // Update the view model based on changes from the game model
-	        // obj should be the new board state
-	        String newBoardState = (String) obj;
-	        // Process the new state as needed for the view
+		 	String newBoardState = (String) obj;
+		    setChanged();
+		    notifyObservers(newBoardState);
 	    }
 	
 }
